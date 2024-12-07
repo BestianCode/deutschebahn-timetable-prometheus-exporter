d60620f0b61c512171112aea545f8ab3c9165523
Oleg Smirnov <oleg@smirnov>
2024-12-07 01:06:02 +0100

    Pipeline

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export DB_CLIENT_ID="..."
-export DB_CLIENT_SECRET="..."
+#export DB_CLIENT_ID="..."
+#export DB_CLIENT_SECRET="..."
 
-export KEEP_MINUTES=30
+#export KEEP_MINUTES=30
 
-export DB_STATION="8004128" # Donnersbergerbrücke
+#export DB_STATION="8004128" # Donnersbergerbrücke
 #export DB_STATION="8011160" # Berlin Hbf
 
 python main.py
